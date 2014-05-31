cb40e5003098f73162eaccc410856ba12f178a09
Lucas Souza <lucasfs7@gmail.com>
2014-05-31 16:17:55 -0300

    improve vagrant configs

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,9 @@
 Vagrant.configure("2") do |config|
-  config.vm.box = "common-precise32"
+  config.vm.box = "precise32"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
-  config.vm.provision :shell, :path => "script/bootstrap.sh"
+  config.vm.provision :shell, :path => "bootstrap.sh"
   config.vm.network :forwarded_port, guest: 4000, host: 4000
-  config.vm.synced_folder ".", "/vagrant", nfs: true
+  config.vm.synced_folder ".", "/vagrant"
   config.ssh.forward_agent = true
   config.vm.network :private_network, ip: "33.33.33.10"
   config.vm.network :public_network
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,31 +1,14 @@
 #!/usr/bin/env bash
-cd /vagrant
 
-# Update apt repositories
 echo "Updating apt repositories"
 apt-get update
 
-# git
 echo "Installing git"
 sudo apt-get -y install git git-man git-doc gitk
 
-# node.js
 echo "Installing Node.js"
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 sudo apt-get -y install python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get -y install nodejs
-
-# Setup project dependencies
-echo "setting up project dependecies"
-sudo npm install -g coffee-script
-sudo npm install -g handlebars
-sudo npm install -g karma
-sudo npm install -g karma-mocha
-sudo npm install -g karma-coffee-preprocessor
-sudo npm install -g karma-phantomjs-launcher
-sudo npm install -g karma-chrome-launcher
-sudo npm install -g karma-firefox-launcher
-sudo npm install -g karma-safari-launcher
-sudo npm install -g phantomjs
